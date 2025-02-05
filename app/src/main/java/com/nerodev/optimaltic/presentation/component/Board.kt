e6d1491ee673f1e62cdac41238fe3a9e8c2e818f
Nero24 <lolilolipo24@gmail.com>
2025-02-05 12:13:41 +0700

    refactor cell

--- app/src/main/java/com/nerodev/optimaltic/presentation/component/Board.kt
+++ app/src/main/java/com/nerodev/optimaltic/presentation/component/Board.kt
@@ -1,21 +1,8 @@
 package com.nerodev.optimaltic.presentation.component
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.nerodev.optimaltic.domain.model.Player
 
 @Composable
@@ -40,24 +27,3 @@ fun Board(
     }
 }
 
-@Composable
-fun Cell(player: Player, isClickable: Boolean, onClick: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .border(2.dp, Color.Gray, RoundedCornerShape(8.dp))
-            .clickable(enabled = isClickable) { onClick() },
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = when (player) {
-                Player.X -> "X"
-                Player.O -> "O"
-                Player.NONE -> ""
-            },
-            fontSize = 36.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
-    }
-}
